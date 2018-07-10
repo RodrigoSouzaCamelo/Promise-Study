9a5c8d3af17d90e97c21d3c787bd41f9051444b4
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2018-07-09 21:19:02 -0300

    Created method post

--- functions-index.js
+++ functions-index.js
@@ -19,3 +19,25 @@ function get(url) {
         req.send();
     });
 }
+
+function post(url, data) {
+    return new Promise(function (resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('POST', url, true);
+
+        req.onload = function () {
+            if (req.status == 200) {
+                resolve(req.responseText);
+            }
+            else {
+                reject(Error(req.statusText));
+            }
+        };
+
+        req.onerror = function () {
+            reject(Error("Network Error"));
+        };
+
+        req.send(data);
+    });
+}
